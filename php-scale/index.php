3fcac49a8979fddd59347fdee34d6c20b37a866a
psa-devops1 <pierre.saminadin@arrow.com>
2025-05-26 09:11:40 -0400

    ajout image

--- php-scale/index.php
+++ php-scale/index.php
@@ -2,3 +2,5 @@
   ECHO "Hello World!<br>";
   print "I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
 ?>
+
+<img src="https://i.etsystatic.com/12387150/r/il/936af1/5627167411/il_1588xN.5627167411_1sv4.jpg" alt="W3Schools.com">
